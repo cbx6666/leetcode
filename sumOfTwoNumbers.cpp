b32a43a692b6cb3dd6d48bda291ea8ba6ae85c5c
陈柏熙 <2353120@tongji.eud.cn>
2025-02-28 21:11:10 +0800

    2025.2.22 两数之和

---
+++ sumOfTwoNumbers.cpp
@@ -0,0 +1,18 @@
+/*两数之和*/
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+//你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+//你可以按任意顺序返回答案。
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (numMap.find(complement) != numMap.end()) {
+                return { numMap[complement], i };
+            }
+            numMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
