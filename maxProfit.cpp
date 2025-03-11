5bd60ebefe56b5ca0c0ec207989e761e79a57755
陈柏熙 <2353120@tongji.edu.cn>
2025-03-12 01:04:50 +0800

    2025.3.10 买股票的最佳时机

--- maxProfit.cpp
+++ maxProfit.cpp
@@ -1,17 +1,19 @@
-/*���Ʊ�����ʱ��*/
-//����һ������ prices �����ĵ� i ��Ԫ�� prices[i] ��ʾһ֧������Ʊ�� i ��ļ۸�
-//��ֻ��ѡ�� ĳһ�� ������ֻ��Ʊ����ѡ���� δ����ĳһ����ͬ������ �����ù�Ʊ�����һ���㷨�����������ܻ�ȡ���������
-//��������Դ���ʽ����л�ȡ�������������㲻�ܻ�ȡ�κ����󣬷��� 0 ��
+/*买股票的最佳时机*/
+//给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+//你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+//返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int minPrice = INT_MAX;
         int profit = 0;
         int n = prices.size();
+        
         for (int i = 0; i < n; i++) {
             minPrice = min(minPrice, prices[i]);
             profit = max(profit, prices[i] - minPrice);
         }
+        
         return profit;
     }
 };
