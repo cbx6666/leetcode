42e48edfe0e4582432cb22bbe32087ee2f32684d
陈柏熙 <2353120@tongji.edu.cn>
2025-03-24 17:01:56 +0800

    2025.3.6 零钱兑换

--- 动态规划/coinChange.cpp
+++ 动态规划/coinChange.cpp
@@ -1,7 +1,7 @@
-/*��Ǯ�һ�*/
-//����һ���������� coins ����ʾ��ͬ����Ӳ�ң��Լ�һ������ amount ����ʾ�ܽ�
-//���㲢���ؿ��Դճ��ܽ������� ���ٵ�Ӳ�Ҹ��� �����û���κ�һ��Ӳ�����������ܽ����� - 1 ��
-//�������Ϊÿ��Ӳ�ҵ����������޵ġ�
+/*零钱兑换*/
+//给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+//计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 - 1 。
+//你可以认为每种硬币的数量是无限的。
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
@@ -19,6 +19,7 @@ public:
                 dp[i] = min + 1;
             }
         }
+        
         return dp[amount] < INT_MAX ? dp[amount] : -1;
     }
 };
