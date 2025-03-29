fda6bdd92accafa1bcbb632deb8eaf197fd4fa8b
陈柏熙 <2353120@tongji.eud.cn>
2025-03-29 23:11:06 +0800

    2025.3.29 将有序数组转换为二叉搜索树

---
+++ 二叉树/sortedArrayToBST.cpp
@@ -0,0 +1,21 @@
+/*将有序数组转换为二叉搜索树*/
+//给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 平衡 二叉搜索树。
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return BuildBST(nums, 0, nums.size() - 1);
+    }
+
+    TreeNode* BuildBST(vector<int>& nums, int begin, int end) {
+        if (begin > end) {
+            return nullptr;
+        }
+
+        int middle = (begin + end) / 2;
+        TreeNode* root = new TreeNode(nums[middle]);
+        root->left = BuildBST(nums, begin, middle - 1);
+        root->right = BuildBST(nums, middle + 1, end);
+
+        return root;
+    }
+};
