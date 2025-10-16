0e225541e22537f273124566622ab9b6bee8dbe0
陈柏熙 <2353120@tongji.eud.cn>
2025-10-17 01:45:59 +0800

    2025.5.30 编辑距离

--- 多维动态规划/minDistance.cpp
+++ 多维动态规划/minDistance.cpp
@@ -5,29 +5,29 @@
 // 删除一个字符
 // 替换一个字符
 class Solution {
-    public:
-        int minDistance(string word1, string word2) {
-            // 编辑距离的关键性质是：存在一个最优操作序列，其中所有操作都发生在字符串末尾。
-            // 这不是因为实际编辑时只能操作末尾，而是因为任何中间操作都可以等价地"延迟"到末尾处理，而不增加操作次数。
-            int m = word1.size();
-            int n = word2.size();
-            vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-            for (int i = 0; i <= m; i++) {
-                dp[i][0] = i;
-            }
-            for (int i = 0; i <= n; i++) {
-                dp[0][i] = i;
-            }
-            for (int i = 1; i <= m; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (word1[i - 1] == word2[j - 1]) {
-                        dp[i][j] = dp[i - 1][j - 1];
-                    } else {
-                        dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
-                    }
+public:
+    int minDistance(string word1, string word2) {
+        // 编辑距离的关键性质是：存在一个最优操作序列，其中所有操作都发生在字符串末尾。
+        // 这不是因为实际编辑时只能操作末尾，而是因为任何中间操作都可以等价地"延迟"到末尾处理，而不增加操作次数。
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
                 }
             }
-    
-            return dp[m][n];
         }
-    };
+
+        return dp[m][n];
+    }
+};
